735f903bdbcf42fb9d9dc4c2bd0f4a6ac978e5e2
Jony Choi <jony.choi7@gmail.com>
2019-03-20 01:17:55 -0600

    REST API React to Express!!!!! I did it

--- client/src/App.js
+++ client/src/App.js
@@ -20,33 +20,22 @@ const styles = theme => ({
   }
 });
 
-const customer = [
-  {
-    id: 1,
-    image: "https://placeimg.com/64/64/1",
-    name: "Luis Perez",
-    date: "940102",
-    gender: "masculino",
-    occupation: "student"
-  },
-  {
-    id: 2,
-    image: "https://placeimg.com/64/64/2",
-    name: "Jung Ik",
-    date: "990102",
-    gender: "masculino",
-    occupation: "student"
-  },
-  {
-    id: 3,
-    image: "https://placeimg.com/64/64/3",
-    name: "Hugo",
-    date: "930102",
-    gender: "masculino",
-    occupation: "student"
-  }
-];
 class App extends Component {
+  state = {
+    customer: ""
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ customer: res }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch("/api/customers");
+    const body = await response.json();
+    return body;
+  };
   render() {
     const { classes } = this.props;
     return (
@@ -63,19 +52,21 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-            {customer.map(c => {
-              return (
-                <Customer
-                  id={c.id}
-                  name={c.name}
-                  image={c.image}
-                  date={c.date}
-                  gender={c.gender}
-                  occupation={c.occupation}
-                  key={c.id}
-                />
-              );
-            })}
+            {this.state.customer
+              ? this.state.customer.map(c => {
+                  return (
+                    <Customer
+                      id={c.id}
+                      name={c.name}
+                      image={c.image}
+                      date={c.date}
+                      gender={c.gender}
+                      occupation={c.occupation}
+                      key={c.id}
+                    />
+                  );
+                })
+              : ""}
           </TableBody>
         </Table>
       </Paper>
--- server.js
+++ server.js
@@ -7,8 +7,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/api/hello", (req, res) => {
-  res.send({ message: "Hello express" });
+app.get("/api/customers", (req, res) => {
+  res.send([
+    {
+      id: 1,
+      image: "https://placeimg.com/64/64/1",
+      name: "Luis Perez",
+      date: "940102",
+      gender: "masculino",
+      occupation: "student"
+    },
+    {
+      id: 2,
+      image: "https://placeimg.com/64/64/2",
+      name: "Jung Ik",
+      date: "990102",
+      gender: "masculino",
+      occupation: "student"
+    },
+    {
+      id: 3,
+      image: "https://placeimg.com/64/64/3",
+      name: "Hugo",
+      date: "930102",
+      gender: "masculino",
+      occupation: "student"
+    }
+  ]);
 });
 
 app.listen(port, () => {
