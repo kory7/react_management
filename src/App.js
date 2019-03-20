304239c25c4d8cfed4680b01d3034c2019ac7269
Jony Choi <jony.choi7@gmail.com>
2019-03-19 21:42:03 -0600

    Material UI changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import React, { Component } from "react";
 import Customer from "./components/Customer";
 import "./App.css";
+import Table from "@material-ui/core/Table";
+import TableHead from "@material-ui/core/TableHead";
+import TableBody from "@material-ui/core/TableBody";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import { withStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+
+const styles = theme => ({
+  root: {
+    width: "100%",
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+});
+
 const customer = [
   {
     id: 1,
@@ -29,24 +48,39 @@ const customer = [
 ];
 class App extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        {customer.map(c => {
-          return (
-            <Customer
-              id={c.id}
-              name={c.name}
-              image={c.image}
-              date={c.date}
-              gender={c.gender}
-              occupation={c.occupation}
-              key={c.id}
-            />
-          );
-        })}
-      </div>
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>Id</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {customer.map(c => {
+              return (
+                <Customer
+                  id={c.id}
+                  name={c.name}
+                  image={c.image}
+                  date={c.date}
+                  gender={c.gender}
+                  occupation={c.occupation}
+                  key={c.id}
+                />
+              );
+            })}
+          </TableBody>
+        </Table>
+      </Paper>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,43 +1,19 @@
 import React, { Component } from "react";
-
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
 class Customer extends Component {
   render() {
     return (
-      <div>
-        <CustomerProfile
-          name={this.props.name}
-          image={this.props.image}
-          id={this.props.id}
-        />
-        <CustomerInfo
-          date={this.props.date}
-          gender={this.props.gender}
-          occupation={this.props.occupation}
-        />
-      </div>
-    );
-  }
-}
-class CustomerProfile extends Component {
-  render() {
-    return (
-      <div>
-        <img src={this.props.image} alt="Profile" />
-        <h2>
-          {this.props.name} ({this.props.id})
-        </h2>
-      </div>
-    );
-  }
-}
-class CustomerInfo extends Component {
-  render() {
-    return (
-      <div>
-        <p>{this.props.date}</p>
-        <p>{this.props.gender}</p>
-        <p>{this.props.occupation}</p>
-      </div>
+      <TableRow>
+        <TableCell>{this.props.id}</TableCell>
+        <TableCell>
+          <img src={this.props.image} alt="Profile" />
+        </TableCell>
+        <TableCell>{this.props.name}</TableCell>
+        <TableCell>{this.props.date}</TableCell>
+        <TableCell>{this.props.gender}</TableCell>
+        <TableCell>{this.props.occupation}</TableCell>
+      </TableRow>
     );
   }
 }
